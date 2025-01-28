ec88151a19edab151d2b68c61918fd661acfcb71
simjsik <simjsik75@naver.com>
2025-01-28 23:37:09 +0900

    북마크 로직 수정

--- src/app/api/loadToFirebasePostData/fetchPostData.ts
+++ src/app/api/loadToFirebasePostData/fetchPostData.ts
@@ -150,7 +150,7 @@ export const fetchPosts = async (
     }
 };
 
-// 일반 포스트 무한 스크롤 로직
+// 공지사항 포스트 무한 스크롤 로직
 export const fetchNoticePosts = async (
     userId: string | null = null,
     pageParam: [boolean, Timestamp] | [boolean, null],
@@ -250,74 +250,35 @@ export const fetchBookmarks = async (
 ) => {
     if (bookmarkIds.length <= 0) return;
     try {
-        const response = await fetch('http://localhost:3000/api/firebaseLimit', {
+        const LimitResponse = await fetch('http://localhost:3000/api/firebaseLimit', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
                 'user-id': userId || '',
             }
         });
-
-        if (response.status === 403) {
-            throw new Error('사용량 제한 초과');
+        if (LimitResponse.status === 403) {
+            throw new Error('사용량 제한을 초과했습니다. 더 이상 요청할 수 없습니다.');
         }
 
-        // 닉네임 매핑을 위한 캐시 초기화
-        const userCache = new Map<string, { nickname: string; photo: string | null }>();
+        const BookmarkResponse = await fetch('http://localhost:3000/api/loadToFirebasePostData/fetchBookmarks', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'user-id': userId || '',
+            },
+            body: JSON.stringify({ bookmarkIds, startIdx, pageSize }),
+        });
+        if (!BookmarkResponse.ok) {
+            const errorDetails = await BookmarkResponse.json();
+            throw new Error(`포스트 요청 실패: ${errorDetails.message}`);
+        }
 
-        const postIds = bookmarkIds.slice(startIdx, startIdx + pageSize);
+        const bookmarkData = await BookmarkResponse.json()
 
-        console.log(userId, '사용자', startIdx, '첫 포스트 인덱스', postIds, '가져올 북마크 ID')
-        const postWithComment: PostData[] = (
-            await Promise.all(
-                postIds.map(async (postId) => {
-                    // 포스트 가져오기
-                    const postRef = doc(db, "posts", postId);
-                    const postSnap = await getDoc(postRef);
+        const validPosts = bookmarkData.validPosts
+        const nextIndex = bookmarkData.nextIndex
 
-                    if (!postSnap.exists()) return null;
-
-                    const postsData = postSnap.data()
-
-                    postsData.id = postSnap.id; // 문서 ID를 추가
-                    // 댓글 개수 가져오기
-                    const commentRef = collection(db, 'posts', postId, 'comments');
-                    const commentSnapshot = await getDocs(commentRef);
-                    postsData.commentCount = commentSnapshot.size;
-
-                    // 포스트 데이터에 유저 이름 매핑하기
-                    if (!userCache.has(postsData.userId)) {
-                        const userDocRef = doc(db, "users", postsData.userId); // DocumentReference 생성
-                        const userDoc = await getDoc(userDocRef); // 문서 데이터 가져오기
-
-                        const userData = userDoc.exists()
-                            ? userDoc.data()
-                            : { displayName: 'unknown', PhotoURL: null }
-
-                        userCache.set(postsData.userId, {
-                            nickname: userData.displayName,
-                            photo: userData.photoURL || null,
-                        });
-                    }
-
-                    const userData = userCache.get(postsData.userId) || { nickname: 'Unknown', photo: null }
-                    postsData.displayName = userData.nickname;
-                    postsData.PhotoURL = userData.photo;
-
-                    return postsData as PostData;
-                })
-            )
-        ).filter((post): post is PostData => post !== null);
-
-        // null 값 제거 및 타입 확인
-        const validPosts = postWithComment.filter(
-            (post): post is PostData => post !== null
-        );
-
-        const nextIndex =
-            startIdx + pageSize < bookmarkIds.length ? startIdx + pageSize : undefined;
-
-        console.log(validPosts, '보내줄 포스트', nextIndex, '다음 페이지 기준')
         return { data: validPosts, nextIndexData: nextIndex };
     } catch (error: any) {
         console.error('Error in fetchPosts:', error.message);
