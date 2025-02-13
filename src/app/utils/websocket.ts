27ab5993ffda6aa88c1d927327a229a854ed8b6f
simjsik <simjsik75@naver.com>
2025-02-13 16:26:54 +0900

    Vercel 서버리스 함수 테스트용

--- src/app/utils/websocket.ts
+++
@@ -1,8 +0,0 @@
-// client/utils/socket.js
-import { io } from 'socket.io-client';
-
-const socket = io(process.env.NEXT_PUBLIC_SOCKET_URL || '', {
-    transports: ['websocket'], // 웹소켓 전송 방식 사용
-});
-
-export default socket;
