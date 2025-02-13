23766334f2d6e59df346287f5b096bdd79df1845
simjsik <simjsik75@naver.com>
2025-02-13 19:51:28 +0900

    서버리스 함수를 위한 Page.tsx 구조 변경 & 메인 페이지 수정

--- src/app/home/layout.tsx
+++ src/app/home/layout.tsx
@@ -1,87 +1,11 @@
-/** @jsxImportSource @emotion/react */ // 최상단에 배치
-"use client"
-import { ReactNode, useEffect } from 'react';
-import NavBar from '../components/NavBar';
-import UsageLimit from '../components/UsageLimit';
-import LoginBox from '../login/LoginBox';
-import StatusBox from '../components/StatusBox';
-import { useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil';
-import { bookMarkState, hasGuestState, UsageLimitState, UsageLimitToggle, userData, userState } from '../state/PostState';
-import { checkUsageLimit } from '../utils/checkUsageLimit';
-import { doc, getDoc } from 'firebase/firestore';
-import { db } from '../DB/firebaseConfig';
+import { ReactNode } from 'react';
 
 type LayoutProps = {
     children: ReactNode;
 };
 
-function HomeContent({ children }: LayoutProps) {
-    const currentUser = useRecoilValue<userData | null>(userState)
-    const hasGuest = useRecoilValue(hasGuestState)
-    const setCurrentBookmark = useSetRecoilState<string[]>(bookMarkState)
-    const [usageLimit, setUsageLimit] = useRecoilState<boolean>(UsageLimitState)
-    const setLimitToggle = useSetRecoilState<boolean>(UsageLimitToggle)
-
-    // 사용량 확인
-    useEffect(() => {
-        if (currentUser) {
-            const checkLimit = async () => {
-                try {
-                    await checkUsageLimit(currentUser.uid);
-                } catch (err: unknown) {
-                    if (err instanceof Error) {
-                        if (err.message.includes('사용량 제한')) {
-                            setUsageLimit(true);
-                        } else {
-                            console.log('사용량을 불러오는 중 에러가 발생했습니다.');
-                        }
-                    }
-                }
-            }
-
-            const loadBookmarks = async (uid: string) => {
-                if (hasGuest) {
-                    return setCurrentBookmark([]);
-                }
-                try {
-                    const bookmarks = await getDoc(doc(db, `users/${uid}/bookmarks/bookmarkId`));
-                    if (bookmarks.exists()) {
-                        // 북마크 데이터가 있을 경우
-                        const data = bookmarks.data() as { bookmarkId: string[] };
-                        setCurrentBookmark(data.bookmarkId); // Recoil 상태 업데이트
-                    }
-                } catch (error) {
-                    console.error("북마크 데이터를 가져오는 중 오류 발생:", error);
-                    setCurrentBookmark([]);
-                }
-            }
-            loadBookmarks(currentUser.uid);
-
-            checkLimit();
-        }
-    }, [currentUser, hasGuest])
-
-    useEffect(() => {
-        if (usageLimit) {
-            setLimitToggle(true);
-        }
-    }, [usageLimit])
-    return (
-        <>
-            <LoginBox />
-            <StatusBox></StatusBox>
-            <UsageLimit />
-            <NavBar></NavBar>
-            {children}
-        </>
-    );
-}
-
-
 export default function HomeLayout({ children }: LayoutProps) {
     return (
-        <HomeContent>
-            {children}
-        </HomeContent>
+        { children }
     );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from 'react';
 import "./globals.css";
 import { userData } from './state/PostState';
 import loginListener from './hook/LoginHook';
-import ProviderClient from './components/ProviderComponents/ProviderClients';
+import ProviderClient from './ProviderClients';
 
 type LayoutProps = {
   children: ReactNode;
@@ -17,7 +17,6 @@ interface loginData {
 const loginData = await loginListener();
 
 export default async function RootLayout({ children }: LayoutProps) {
-
   return (
     <html lang="ko">
       <body>
