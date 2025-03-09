6cc489f0effe0102ea641efe3ad680962eb5f3b8
simjsik <simjsik75@naver.com>
2025-03-09 23:24:24 +0900

    자동 로그인 유저 정보 타입 명시

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import { ReactNode } from 'react';
 import "./globals.css";
 import loginListener from './hook/LoginHook';
 import ProviderClient from './ProviderClients';
+import { userData } from './state/PostState';
 
 export const dynamic = "force-dynamic";
 
@@ -10,7 +11,8 @@ type LayoutProps = {
 };
 
 export default async function RootLayout({ children }: LayoutProps) {
-  const loginData = await loginListener();
+  const autologin = await loginListener();
+  const loginData = { user: autologin.user as userData, hasLogin: autologin.hasLogin as boolean, hasGuest: autologin.hasGuest as boolean }
 
   return (
     <html lang="ko">
