1924d7dece13092213a07c1e6bf257081bab71a5
simjsik <simjsik75@naver.com>
2025-03-13 19:08:35 +0900

    로그아웃 API | 클라이언트 측 API 요청 루트 수정

--- src/app/components/Logout.tsx
+++ src/app/components/Logout.tsx
@@ -49,7 +49,7 @@ export default function Logout() {
         try {
             const confirmed = confirm('로그아웃 하시겠습니까?')
             if (confirmed && user) {
-                const response = await fetch("/api/logout", {
+                const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/logout`, {
                     method: "POST",
                     headers: {
                         "Content-Type": "application/json",
