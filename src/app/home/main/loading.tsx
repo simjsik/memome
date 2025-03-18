3b3bc6c38f01952d5e6aca3beb9eaf901a8d39cf
simjsik <simjsik75@naver.com>
2025-03-18 17:35:21 +0900

    로딩 UI | 페이지 전환 시 CSS 수정

--- src/app/home/main/loading.tsx
+++ src/app/home/main/loading.tsx
@@ -2,10 +2,23 @@
 "use client";
 
 import { PostWrap } from "@/app/styled/PostComponents";
+import { css } from "@emotion/react";
 import { BeatLoader } from "react-spinners";
 
 export default function LoadingWrap() {
     return (
-        <PostWrap><BeatLoader color="red" size={8} /></PostWrap>
+        <PostWrap><BeatLoader color="red" size={8} />
+            <div css={css`
+                background-color : red});
+                position : absolute;
+                left : 0;
+                top : 0;
+                bottom : 0;
+                right :0;
+                width : 100%;
+                height : 100%;
+                z-index : 10000;
+                `}></div>
+        </PostWrap>
     )
 }
