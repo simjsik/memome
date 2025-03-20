f4a22298919a05a37d3452b537a83822114e3224
simjsik <simjsik75@naver.com>
2025-03-20 16:35:39 +0900

    북마크 UI | 북마크 애니메이션 수정

--- src/app/components/BookmarkBtn.tsx
+++ src/app/components/BookmarkBtn.tsx
@@ -76,64 +76,62 @@ export default function BookmarkBtn({ postId }: PostId) {
                             throw new Error('알 수 없는 에러 - 북마크 추가 실패');
                         }
                     })
-                alert('북마크 추가 완료!');
                 setCurrentBookmark((prev) => [...prev, postId]);
             } else {
-                const deleteBookmark = confirm('북마크를 해제 하시겠습니까?')
+                await updateDoc(bookmarkRef, {
+                    bookmarkId: arrayRemove(postId),
+                }); // 배열 형태일 때 현재 포스트 id 삭제.
+                setBookmarked(false);
 
-                if (deleteBookmark) {
-                    await updateDoc(bookmarkRef, {
-                        bookmarkId: arrayRemove(postId),
-                    }); // 배열 형태일 때 현재 포스트 id 삭제.
-                    setBookmarked(false);
+                // 북마크 해제 후 북마크 state 업데이트
+                setCurrentBookmark((prev) => prev.filter((id) => id !== postId));
 
-                    // 북마크 해제 후 북마크 state 업데이트
-                    setCurrentBookmark((prev) => prev.filter((id) => id !== postId));
+                // **userBookmarks에서 해당 포스트 제거**
+                setUserBookmarks((prev) =>
+                    prev.filter((post) => post.id !== postId)
+                );
 
-                    // **userBookmarks에서 해당 포스트 제거**
-                    setUserBookmarks((prev) =>
-                        prev.filter((post) => post.id !== postId)
-                    );
+                // 캐싱된 데이터를 수동으로 업데이트
+                queryClient.setQueryData<BookmarkCache>(['bookmarks', currentUser.uid], (oldData) => {
+                    // console.log("Before update:", oldData); // 업데이트 이전 데이터 확인
+                    if (!oldData) return oldData;
 
-                    // 캐싱된 데이터를 수동으로 업데이트
-                    queryClient.setQueryData<BookmarkCache>(['bookmarks', currentUser.uid], (oldData) => {
-                        // console.log("Before update:", oldData); // 업데이트 이전 데이터 확인
-                        if (!oldData) return oldData;
+                    const newData = {
+                        ...oldData,
+                        pages: oldData.pages.map((page) => {
+                            // console.log("Before filter:", page.data.pages); // 필터링 전 데이터 확인
+                            const filteredData = page.data.filter((post: PostData) => post.id !== postId);
+                            // console.log("After filter:", filteredData); // 필터링 후 데이터 확인
+                            return { ...page, data: filteredData };
+                        }),
+                    };
 
-                        const newData = {
-                            ...oldData,
-                            pages: oldData.pages.map((page) => {
-                                // console.log("Before filter:", page.data.pages); // 필터링 전 데이터 확인
-                                const filteredData = page.data.filter((post: PostData) => post.id !== postId);
-                                // console.log("After filter:", filteredData); // 필터링 후 데이터 확인
-                                return { ...page, data: filteredData };
-                            }),
-                        };
-
-                        // console.log("After update:", newData.pages); // 업데이트 이후 데이터 확인
-                        return newData;
-                    });
-
-                    alert('북마크가 해제되었습니다.');
-                }
+                    // console.log("After update:", newData.pages); // 업데이트 이후 데이터 확인
+                    return newData;
+                });
             }
         }
     }
 
     const MotionBookmark = motion(Bookmark);
+    const MotionBookmarkSVG = motion.svg;
+    const MotionBookmarkPath = motion.path;
 
+    const pathVariants = {
+        initial: { fill: "none", stroke: "#ccc" },
+        onHover: { fill: "none", stroke: "#191919", transition: { duration: 0.3 } },
+        offHover: { fill: "#0087ff", stroke: "none", transition: { duration: 0.3 } },
+    };
     // function
     return (
         <MotionBookmark
             as={motion.button}
             whileHover={{
                 scale: 1.05,
-                backgroundColor: "#3b82f6",
                 transition: { duration: 0.3 },
             }}
             whileTap={{
                 scale: 0.95,
-                rotate: "2deg",
                 transition: { duration: 0.1 },
             }}
             onHoverEnd={() => {
@@ -148,25 +146,35 @@ export default function BookmarkBtn({ postId }: PostId) {
                 });
             }}
             onClick={(event) => { event.preventDefault(); event.stopPropagation(); addBookmark(postId); }}>
-            <svg width="28" height="28" viewBox="0 0 38 38">
+            <MotionBookmarkSVG width="28" height="28" viewBox="0 0 38 38">
                 <g>
                     {bookmarked ?
                         <>
-                            <path d="M9,9.163V28.815a1.31,1.31,0,0,0,.637,1,1.292,1.292,0,0,0,1.181.068l7.691-4.811a1.445,1.445,0,0,1,1,0l7.673,4.811a1.292,1.292,0,0,0,1.181-.068,1.31,1.31,0,0,0,.637-1V9.163A1.249,1.249,0,0,0,27.691,8H10.309A1.249,1.249,0,0,0,9,9.163Z" fill="#0087ff" stroke="#0087ff" strokeWidth="2.5">
-                            </path>
+                            <MotionBookmarkPath d="M9,9.163V28.815a1.31,1.31,0,0,0,.637,1,1.292,1.292,0,0,0,1.181.068l7.691-4.811a1.445,1.445,0,0,1,1,0l7.673,4.811a1.292,1.292,0,0,0,1.181-.068,1.31,1.31,0,0,0,.637-1V9.163A1.249,1.249,0,0,0,27.691,8H10.309A1.249,1.249,0,0,0,9,9.163Z"
+                                fill="#0087ff" stroke="#0087ff" strokeWidth="2.5"
+                                variants={pathVariants}
+                                initial="initial"
+                                whileHover="hover"
+                            >
+                            </MotionBookmarkPath>
                             <rect width="28" height="28" fill="none" stroke="none">
                             </rect>
                         </>
                         :
                         <>
-                            <path d="M9,9.163V28.815a1.31,1.31,0,0,0,.637,1,1.292,1.292,0,0,0,1.181.068l7.691-4.811a1.445,1.445,0,0,1,1,0l7.673,4.811a1.292,1.292,0,0,0,1.181-.068,1.31,1.31,0,0,0,.637-1V9.163A1.249,1.249,0,0,0,27.691,8H10.309A1.249,1.249,0,0,0,9,9.163Z" fill="none" stroke="#ccc" strokeWidth="2.5">
-                            </path>
+                            <MotionBookmarkPath d="M9,9.163V28.815a1.31,1.31,0,0,0,.637,1,1.292,1.292,0,0,0,1.181.068l7.691-4.811a1.445,1.445,0,0,1,1,0l7.673,4.811a1.292,1.292,0,0,0,1.181-.068,1.31,1.31,0,0,0,.637-1V9.163A1.249,1.249,0,0,0,27.691,8H10.309A1.249,1.249,0,0,0,9,9.163Z"
+                                fill="none" stroke="#ccc" strokeWidth="2.5"
+                                variants={pathVariants}
+                                initial="initial"
+                                whileHover="hover"
+                            >
+                            </MotionBookmarkPath>
                             <rect width="28" height="28" fill="none" stroke="none">
                             </rect>
                         </>
                     }
                 </g>
-            </svg>
+            </MotionBookmarkSVG>
         </MotionBookmark>
     )
 }
