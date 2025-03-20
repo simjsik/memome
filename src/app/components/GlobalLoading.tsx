ce0ca84b554770fdfd095c237030168ea2d476c3
simjsik <simjsik75@naver.com>
2025-03-20 14:47:19 +0900

    로딩 UI 디버깅 5

--- src/app/components/GlobalLoading.tsx
+++ src/app/components/GlobalLoading.tsx
@@ -1,6 +1,8 @@
 /** @jsxImportSource @emotion/react */ // 최상단에 배치
 "use client";
 import { MoonLoader } from "react-spinners";
+import { loadingState } from "../state/PostState";
+import { useRecoilValue } from "recoil";
 import styled from "@emotion/styled";
 
 const LoadingWrap = styled.div`
@@ -9,6 +11,7 @@ const LoadingWrap = styled.div`
     left: 500px;
     top: 0;
     bottom: 0;
+    z-index: 10;
   >span{
     position: absolute;
     left: 50%;
@@ -18,12 +21,15 @@ const LoadingWrap = styled.div`
 `
 
 export default function GlobalLoadingWrap() {
+    const loading = useRecoilValue(loadingState);
 
     return (
         <>
+            {loading &&
                 <LoadingWrap>
                     <MoonLoader color="#0087ff" />
                 </LoadingWrap >
+            }
         </>
     )
 }
