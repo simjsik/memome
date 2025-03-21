623b559fd1f1885a7483c27bf77551716a613ba8
simjsik <simjsik75@naver.com>
2025-03-21 20:13:42 +0900

    메인 페이지 무한 스크롤 로직 디버깅 3

--- src/app/home/main/MainClient.tsx
+++ src/app/home/main/MainClient.tsx
@@ -163,18 +163,32 @@ export default function MainHome() {
     retry: false, // 재시도 방지
     queryKey: ['posts'],
     queryFn: async ({ pageParam }) => {
-      const validateResponse = await fetch(`/api/validate`, {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        credentials: "include",
-        body: JSON.stringify({ uid }),
-      });
-      if (!validateResponse.ok) {
-        const errorDetails = await validateResponse.json();
-        throw new Error(`포스트 요청 실패: ${errorDetails.message}`);
+      try {
+        console.log('일반 포스트 요청')
+        setDataLoading(true);
+        const validateResponse = await fetch(`/api/validate`, {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          credentials: "include",
+          body: JSON.stringify({ uid }),
+        });
+        if (!validateResponse.ok) {
+          const errorDetails = await validateResponse.json();
+          throw new Error(`포스트 요청 실패: ${errorDetails.message}`);
+        }
+
+        return await fetchPosts(uid, pageParam, 4);
+      } catch (error: unknown) {
+        if (error instanceof Error) {
+          console.error("일반 오류 발생:", error.message);
+          throw error;
+        } else {
+          console.error("알 수 없는 에러 유형:", error);
+          throw new Error("알 수 없는 에러가 발생했습니다.");
+        }
+      } finally {
+        setDataLoading(false);
       }
-      setDataLoading(false);
-      return await fetchPosts(uid, pageParam, 4);
     },
     getNextPageParam: (lastPage) => lastPage.nextPage,
     staleTime: 5 * 60 * 1000,
@@ -224,7 +238,7 @@ export default function MainHome() {
 
     const obsever = new IntersectionObserver(
       (entries) => {
-        if (entries[0].isIntersecting && hasNextPage) {
+        if (entries[0].isIntersecting && hasNextPage && !dataLoading) {
           fetchNextPage();
         }
       },
@@ -557,7 +571,7 @@ export default function MainHome() {
             </motion.div >
           ))
           }
-          {!dataLoading && < div ref={observerLoadRef} style={{ height: '1px' }} />}
+          <div ref={observerLoadRef} style={{ height: '1px', visibility: dataLoading ? "hidden" : "visible" }} />
           {(!loading && dataLoading) && <LoadingWrap />}
           {
             (!hasNextPage && !dataLoading && !loading) &&
