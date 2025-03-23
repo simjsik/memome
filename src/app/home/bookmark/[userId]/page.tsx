2c39a8950a1207562bb4a56646750b77277190c7
simjsik <simjsik75@naver.com>
2025-03-23 15:16:57 +0900

    북마크 페이지 | 메타 데이터 수정

--- src/app/home/bookmark/[userId]/page.tsx
+++ src/app/home/bookmark/[userId]/page.tsx
@@ -3,8 +3,8 @@ import { BookmarkWrap } from '@/app/styled/PostComponents';
 export const dynamic = "force-static"; // 정적 렌더링 설정
 
 export const metadata = {
-    title: "MEMOME :: 홈",
-    description: "최근 공유된 메모를 확인해보세요.",
+    title: "MEMOME :: 북마크",
+    description: "북마크된 메모를 확인해보세요.",
 };
 
 export default async function Bookmark() {
