cea43547f6441dda480cf840eef07574103d2077
simjsik <simjsik75@naver.com>
2025-04-08 22:23:19 +0900

    전체 로딩 UI | 반응형 적용

--- src/app/components/GlobalLoading.tsx
+++ src/app/components/GlobalLoading.tsx
@@ -9,7 +9,7 @@ import { usePathname } from "next/navigation";
 const LoadingWrap = styled.div`
     position: absolute;
     width: 600px;
-    left: 500px;
+    left : clamp(80px, calc(80px + (100vw - 768px) * 0.5), 25%);
     top: 0;
     bottom: 0;
     z-index: 10;
@@ -17,6 +17,26 @@ const LoadingWrap = styled.div`
     position: absolute;
     top: 45%;
     margin : 0 auto;
+  }
+  @media (max-width: 768px) {
+    left: 80px;
+    width: calc(100% - 80px);
+    max-width: 600px;
+  }
+  @media (min-width: 1200px) and (max-width : 1920px) {
+    left : clamp(80px, calc(80px + (100vw - 1200px) * 0.53), 50vw);
+  }
+
+  @media (min-width: 1921px) {
+    width: clamp(600px, calc(600px + (100vw - 1920px) * 0.3125), 800px);
+  }
+
+  @media (min-width: 2560px) {
+    width: clamp(800px, calc(800px + (100vw - 2560px) * 0.3125), 1200px);
+  }
+
+  @media (min-width: 3840px) {
+    width: clamp(1200px, calc(1200px + (100vw - 3840px) * 0.3125), 1600px);
   }
 `
 
