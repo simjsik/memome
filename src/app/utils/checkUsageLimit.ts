5d7a278874a104be4ed20258ef7058748f5df896
simjsik <simjsik75@naver.com>
2025-05-31 15:38:50 +0900

    포스트 상세 페이지 사용량 로직 수정

--- src/app/utils/checkUsageLimit.ts
+++ src/app/utils/checkUsageLimit.ts
@@ -1,6 +1,6 @@
 export const checkUsageLimit = async (userId: string) => {
     try {
-        const LimitResponse = await fetch(`api/limit`, {
+        const LimitResponse = await fetch(`/api/limit`, {
             method: 'POST',
             headers: { "Content-Type": "application/json", 'Project-Host': window.location.origin },
             credentials: "include",
@@ -11,6 +11,9 @@ export const checkUsageLimit = async (userId: string) => {
         }
         if (LimitResponse.status === 403) {
             throw new Error('데이터를 요청할 수 없습니다.');
+        }
+        if (LimitResponse.status === 404) {
+            throw new Error('사용량 확인 요청 실패');
         }
 
         console.log('사용량 요청 확인', userId);
