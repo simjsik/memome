b3b0947ac3e3f8c63f5559220b6a27d565ca3f17
simjsik <simjsik75@naver.com>
2025-08-08 18:39:04 +0900

    포스트 더보기 버튼 svg 수정

--- src/app/home/main/MainClient.tsx
+++ src/app/home/main/MainClient.tsx
@@ -349,9 +349,9 @@ export default function MainHome() {
                     >
                       <svg viewBox="0 0 40 40">
                         <g>
-                          <path css={css`stroke : ${theme.colors.text}; stroke-width: 0.5;`} d="M12.57,21.5a1.5,1.5,0,0,0,0-3,1.5,1.5,0,0,0,0,3Z" />
-                          <path css={css`stroke : ${theme.colors.text}; stroke-width: 0.5;`} d="M20,21.5a1.5,1.5,0,0,0,0-3,1.5,1.5,0,0,0,0,3Z" />
-                          <path css={css`stroke : ${theme.colors.text}; stroke-width: 0.5;`} d="M27.43,21.5a1.5,1.5,0,0,0,0-3,1.5,1.5,0,0,0,0,3Z" />
+                          <path css={css`fill: ${theme.colors.text}; stroke : ${theme.colors.text}; stroke-width: 0.5;`} d="M12.57,21.5a1.5,1.5,0,0,0,0-3,1.5,1.5,0,0,0,0,3Z" />
+                          <path css={css`fill: ${theme.colors.text}; stroke : ${theme.colors.text}; stroke-width: 0.5;`} d="M20,21.5a1.5,1.5,0,0,0,0-3,1.5,1.5,0,0,0,0,3Z" />
+                          <path css={css`fill: ${theme.colors.text}; stroke : ${theme.colors.text}; stroke-width: 0.5;`} d="M27.43,21.5a1.5,1.5,0,0,0,0-3,1.5,1.5,0,0,0,0,3Z" />
                         </g>
                       </svg>
                       {dropToggle === post.id &&
