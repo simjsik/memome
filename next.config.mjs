7562d12b90074b0a8052d78d9122e66cad100383
simjsik <simjsik75@naver.com>
2025-10-31 20:06:35 +0900

    엔드포인트 메소드 수정

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['res.cloudinary.com'],  // ← 여기에 허용할 도메인 추가
+        domains: ['res.cloudinary.com'],
     },
     async headers() {
         return [
             {
                 source: "/api/:path*",
                 headers: [
-                    // 개발 환경: localhost 허용
                     { key: "Access-Control-Allow-Origin", value: process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://memome-delta.vercel.app" },
                     { key: "Access-Control-Allow-Credentials", value: "true" },
-                    { key: "Access-Control-Allow-Methods", value: "GET,POST,OPTIONS,PUT,DELETE" },
+                    { key: "Access-Control-Allow-Methods", value: "GET,POST,DELETE" },
                     { key: "Access-Control-Allow-Headers", value: "Content-Type,Authorization,x-user-uid,x-project-host" },
                 ],
             },
