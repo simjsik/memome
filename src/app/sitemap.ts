a22bb922938514da71998bdda104087621c073da
simjsik <simjsik75@naver.com>
2025-11-26 19:20:04 +0900

    크롤러 설정 수정

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -9,7 +9,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
             priority: 1.0,
         },
         {
-            url: 'https://memome-delta.vercel.app/login',
+            url: 'https://memome-delta.vercel.app/home/notice',
             lastModified: new Date(),
             changeFrequency: 'monthly',
             priority: 0.8,
